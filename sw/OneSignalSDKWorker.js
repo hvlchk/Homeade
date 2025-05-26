fad3f73227b44e45452daf6a6db1a97f7f46620d
hvlchk <c.hvlchk@gmail.com>
2025-05-26 16:32:28 -0700

    Add files via upload

---
+++ sw/OneSignalSDKWorker.js
@@ -0,0 +1,25 @@
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+
+// ✅ Custom behavior: focus or open installed PWA window on notification click
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+
+  const urlToOpen = new URL('/', self.location.origin).href;
+
+  event.waitUntil(
+    clients.matchAll({
+      type: 'window',
+      includeUncontrolled: true
+    }).then(function(clientList) {
+      for (let client of clientList) {
+        if (client.url === urlToOpen && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow(urlToOpen);
+      }
+    })
+  );
+});
